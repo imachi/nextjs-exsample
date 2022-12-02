5e21645578fb1703d6e2747aa46f9d15d327f7ca
今泉千尋 <imaizumichihiro@imaizumichihironoMacBook-Pro.local>
2022-12-02 23:05:56 +0900

    add widget component

---
+++ src/components/Widget.tsx
@@ -0,0 +1,15 @@
+import { PopularContents } from './PoplarContents';
+
+export const Widget = () => {
+	return (
+		<div
+			style={{
+				backgroundColor: '#E14D2A',
+				marginRight: '30px',
+				padding: '20px'
+			}}
+		>
+			<PopularContents />
+		</div>
+	);
+};
