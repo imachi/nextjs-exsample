f6ae13e688f2eae2321ff3e753dcbb29ad31489f
今泉千尋 <imaizumichihiro@imaizumichihironoMacBook-Pro.local>
2022-12-02 23:06:07 +0900

    add pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,46 @@
+import { use } from 'react';
+
+type Article = {
+	title: string;
+	description: string;
+};
+
+async function getData() {
+	const res = await fetch('http://localhost:3000/api/articles');
+	return res.json();
+}
+
 export default function Page() {
-	return <div>Hello World</div>;
+	const articles: Article[] = use(getData());
+
+	return (
+		<div
+			style={{
+				width: '75vw',
+				margin: 'auto',
+				backgroundColor: '#FD841F',
+				display: 'flex',
+				padding: '30px',
+			}}
+		>
+			{articles.map((a) => {
+				return (
+					<div
+						key={a.title}
+						style={{
+							width: '20%',
+							backgroundColor: 'white',
+							padding: '20px',
+							borderRadius: '10px',
+							boxShadow: '2px 2px 5px 2px rgba(0, 0, 0, 0.2)',
+							margin: '10px',
+						}}
+					>
+						<h3>{a.title}</h3>
+						<p>{a.description}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
