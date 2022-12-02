6a238ea09399ec7ea12d34d902abce536438a021
今泉千尋 <imaizumichihiro@imaizumichihironoMacBook-Pro.local>
2022-12-02 23:05:20 +0900

    add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,34 @@
+import { Header } from 'components/Header';
+import { Widget } from 'components/Widget';
+
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html>
+			<head />
+			<body
+				style={{
+					background: '#3E6D9C',
+					margin: '0',
+				}}
+			>
+				<Header />
+				<div
+					style={{
+						display: 'flex',
+						height: '80vh',
+						width: '95%',
+						margin: 'auto',
+						marginTop: '20px',
+					}}
+				>
+					<Widget />
+					<div>{children}</div>
+				</div>
+			</body>
+		</html>
+	);
 }
